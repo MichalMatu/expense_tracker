842b6b56b4c5fa478941173f1f7ebcccbd0776a3
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-14 22:33:41 +0200

    dummy data creted

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expensed extends StatefulWidget {
   const Expensed({super.key});
@@ -10,6 +11,70 @@ class Expensed extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expensed> {
+  final List<Expense> _registredExpenses = [
+    Expense(
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Weekly Groceries',
+      amount: 103.45,
+      // set date to yesterday
+      date: DateTime.now().subtract(const Duration(days: 1)),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'New Phone',
+      amount: 799.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'New Desk',
+      amount: 199.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'New Monitor',
+      amount: 299.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'New Keyboard',
+      amount: 99.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'New Mouse',
+      amount: 49.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'New Chair',
+      amount: 199.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'New Lamp',
+      amount: 29.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'New Plant',
+      amount: 9.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
