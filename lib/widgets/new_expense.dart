b9ec8dacee43c3f8e2465d94e0fcbc31bae67bd0
MichalMatu <michal.matuszewski1984@gmail.com>
2024-10-15 23:35:46 +0200

    date input formatted

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -13,6 +13,18 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
 
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+    showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -30,11 +42,30 @@ class _NewExpenseState extends State<NewExpense> {
           maxLength: 50,
           decoration: const InputDecoration(label: Text('Title')),
         ),
-        TextField(
-          controller: _amountController,
-          decoration:
-              const InputDecoration(label: Text('Amount'), prefixText: '\$'),
-          keyboardType: TextInputType.number,
+        Row(
+          children: [
+            Expanded(
+                child: TextField(
+              controller: _amountController,
+              decoration: const InputDecoration(
+                  label: Text('Amount'), prefixText: '\$'),
+              keyboardType: TextInputType.number,
+            )),
+            const SizedBox(width: 8),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text('Date'),
+                  IconButton(
+                    onPressed: _presentDatePicker,
+                    icon: const Icon(Icons.calendar_month),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
         Row(children: [
           TextButton(
